395b68b1df30f0a1c40acf036f16c23d00793efd
Priyanka Maheshwari <33982352+priyankama@users.noreply.github.com>
2019-05-21 12:58:13 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.sociallogin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.facebook.login.LoginManager
+import com.facebook.CallbackManager
+import java.util.*
+
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    private var callbackManager: CallbackManager? = null
+    public var TAG= "MainActivity"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       /*
+        to generate hash key:unique for each system
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i(TAG, "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e(TAG, "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e(TAG, "printHashKey()", e)
+        }*/
+
+
+        var btnLoginFacebook = findViewById<Button>(R.id.login_button)
+
+        btnLoginFacebook.setOnClickListener(View.OnClickListener {
+            // Login
+            callbackManager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+            LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(loginResult: LoginResult) {
+                        // App code
+                        Log.d("letsSee", "Facebook token: " + loginResult.accessToken.token)
+
+                    }
+
+                    override fun onCancel() {
+                        // App code
+                        Log.d("onCancel", "Facebook onCancel.")
+
+                    }
+
+                    override fun onError(exception: FacebookException) {
+                        // App code
+                        Log.d("onError", "Facebook onError.")
+
+                    }
+                })
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager!!.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("result", "On Activity Result : " + data)
+    }
+}
